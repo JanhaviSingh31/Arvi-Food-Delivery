fd49e42794a4b21b57766bca6f5293f3e933eddb
JanhaviSingh31 <145059543+JanhaviSingh31@users.noreply.github.com>
2024-05-06 21:00:39 +0530

    Home

---
+++ TopMeal.js
@@ -0,0 +1,41 @@
+export const TopMeal=[
+    {
+        image:"https://cdn.pixabay.com/photo/2024/04/23/09/32/ai-generated-8714516_640.jpg",
+    title:"Pizza"
+
+},
+{
+    image:"https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg",
+    title:"Burger"
+},
+{
+    image:"https://cdn.pixabay.com/photo/2015/04/08/13/14/pizza-712667_640.jpg",
+    title:"Rolls"
+},
+
+{
+    image:"https://cdn.pixabay.com/photo/2017/05/31/02/56/food-photography-2358904_640.jpg",
+    title:"Sandwich"
+},
+
+{
+ image:"https://cdn.pixabay.com/photo/2021/07/15/19/45/spaghetti-6469293_640.png",
+ title:"Noodles"
+},
+{
+    image:"https://cdn.pixabay.com/photo/2017/04/04/17/33/food-2202358_640.jpg",
+    title:"Chicken"
+},
+{
+    image:"https://cdn.pixabay.com/photo/2016/02/16/14/53/fish-and-chips-1203400_640.jpg",
+    title:"Fish Fry"
+},
+{
+    image:"https://cdn.pixabay.com/photo/2016/11/20/09/06/bowl-1842294_640.jpg",
+    title:"French Fries"
+},
+{
+    image:"https://media.istockphoto.com/id/1303091718/photo/kerala-vegetarian-foods-dosa-with-curries.jpg?s=612x612&w=0&k=20&c=fMaq0nQaMUjKYe6XkMLJYUCshkMF5jdPBU-O0RYT3X0=",
+    title:"Dosa"
+}
+]
